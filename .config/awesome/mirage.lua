f2b4bd1c00794c68c8af2e818a3b2dc1c11beba4
Marcos Souza <marcos@marcos.com>
2023-12-13 17:06:18 -0300

    chg wall

--- .config/awesome/mirage.lua
+++ .config/awesome/mirage.lua
@@ -23,7 +23,7 @@ local mirage = {}
 
 mirage.initialize = function()
    -- Set Wallpaper
-   gears.wallpaper.maximized(gears.filesystem.get_configuration_dir() .. "/wallpaper/mountain.jpg")
+   gears.wallpaper.maximized(gears.filesystem.get_configuration_dir() .. "/wallpaper/abstract.jpg")
 
    -- Import components
    require("components.exit-screen")
