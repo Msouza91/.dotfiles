d1ec8a91e2f6ae9f70f8595bf3feafdf2e2e5045
Marcos Souza <marcos@marcos.com>
2023-12-21 13:25:12 -0300

    dependency redundancy removed

--- .config/nvim/lua/marcos/packer.lua
+++ .config/nvim/lua/marcos/packer.lua
@@ -6,7 +6,6 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
-    use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
     use 'nvim-tree/nvim-web-devicons'
     use 'Bekaboo/deadcolumn.nvim'
     use 'tridactyl/vim-tridactyl'
