6799d66beca8ce51ea104757057093ec25b6085d
Marcos Souza <marcos@marcos.com>
2023-12-23 15:26:40 -0300

    quickfix binds and formatter

--- .config/nvim/lua/marcos/core/remap.lua
+++ .config/nvim/lua/marcos/core/remap.lua
@@ -2,9 +2,11 @@ vim.g.mapleader = " "
 -- Netrw
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Move selected line / block of text in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Better navigation
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
@@ -23,6 +25,7 @@ vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 -- replace word under cursor
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
 -- grant execute permission
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
@@ -36,6 +39,15 @@ end)
 vim.keymap.set("n", "<leader>l", function()
     vim.cmd("Lazy")
 end)
+
+-- Lsp Formatting
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- Quickfix navigation
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- Obsidian
 vim.keymap.set("n", "<leader>o", function()
