bb0e98265232364aad087c0023eca3205f3c693f
Marcos Souza <marcos@marcos.com>
2023-12-23 10:46:52 -0300

    colorizer and fzf native for telescope

--- .config/nvim/lua/marcos/plugins/telescope.lua
+++ .config/nvim/lua/marcos/plugins/telescope.lua
@@ -2,7 +2,10 @@ return {
     'nvim-telescope/telescope.nvim',
     version = '0.1.5', -- or, branch = '0.1.x',
     event = 'VimEnter',
-    dependencies = {'nvim-lua/plenary.nvim'},
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
     config = function()
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
