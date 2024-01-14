ede9a2d97276a2f49a2ceb1534919269231c75de
Marcos Souza <marcos@marcos.com>
2024-01-14 11:48:45 -0300

    feat: nicer netrw

--- .config/nvim/lua/marcos/core/set.lua
+++ .config/nvim/lua/marcos/core/set.lua
@@ -2,6 +2,13 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+vim.g.netrw_banner = 0
+vim.g.netrw_browse_split = 4
+vim.g.netrw_altv = 1
+vim.g.netrw_liststyle = 3
+vim.g.netrw_winsize = 25
+vim.g.netrw_list_hide = "netrw_gitignore#Hide()" .. ",\\(^\\|\\s\\s\\)\\zs\\.\\S\\+"
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
