0acd33b70493eb754b1ef78722df6c5a48bf0263
Marcos Souza <marcos@marcos.com>
2024-01-14 09:38:28 -0300

    fix priority call for the theme before lualine

--- .config/nvim/lua/marcos/plugins/themes.lua
+++ .config/nvim/lua/marcos/plugins/themes.lua
@@ -1,6 +1,8 @@
 return {
 	"rose-pine/neovim",
-	event = "VimEnter",
+	name = "rose-pine",
+	lazy = false,
+	priority = 1000,
 	config = function()
 		vim.cmd("colorscheme rose-pine")
 		--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
