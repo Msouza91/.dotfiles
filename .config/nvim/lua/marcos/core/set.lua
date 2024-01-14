455f3430cf665c7c11ec8734116c2b443e0c7114
Marcos Souza <marcos@marcos.com>
2024-01-14 12:12:57 -0300

    don't auto split >.<''

--- .config/nvim/lua/marcos/core/set.lua
+++ .config/nvim/lua/marcos/core/set.lua
@@ -3,7 +3,7 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 
 vim.g.netrw_banner = 0
-vim.g.netrw_browse_split = 4
+vim.g.netrw_browse_split = 0
 vim.g.netrw_altv = 1
 vim.g.netrw_liststyle = 3
 vim.g.netrw_winsize = 25
