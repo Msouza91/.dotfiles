eaaddcf64fdb986cd37f6a6d05d8a066c6823b85
Marcos Souza <marcos@marcos.com>
2024-01-16 14:13:02 -0300

    quick rearrange with treesitter

--- .config/nvim/lua/marcos/core/remap.lua
+++ .config/nvim/lua/marcos/core/remap.lua
@@ -23,6 +23,9 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+-- TreeSJ
+vim.keymap.set("n", "<leader>m", "<CMD>TSJToggle<CR>")
+
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
