34d7f7f1b4063cdb939b1a0d9530c2d6b3f46eec
Marcos Souza <marcos@marcos.com>
2024-02-09 09:03:57 -0300

    feat: smaller tabs ~soydev~ style because they are prettier

--- .config/nvim/lua/marcos/core/set.lua
+++ .config/nvim/lua/marcos/core/set.lua
@@ -5,9 +5,9 @@ vim.opt.foldlevelstart = 1
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
