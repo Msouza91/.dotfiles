62dcb2f6655345451e88b0ed30b8e02c7dcba7fb
Marcos Souza <marcos@marcos.com>
2024-02-09 12:50:11 -0300

    fix: add event for treesj

--- .config/nvim/lua/marcos/plugins/treesj.lua
+++ .config/nvim/lua/marcos/plugins/treesj.lua
@@ -1,6 +1,7 @@
 return {
 	"Wansmer/treesj",
 	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		require("treesj").setup({ use_default_keymaps = false })
 	end,
