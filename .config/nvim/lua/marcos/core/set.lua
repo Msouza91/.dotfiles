88b788e89edb8066af7ed58d4de98c5c0c8992c4
Marcos Souza <marcos@marcos.com>
2024-02-23 18:04:32 -0300

    feat: some neovide BS for when I'm on windows

--- .config/nvim/lua/marcos/core/set.lua
+++ .config/nvim/lua/marcos/core/set.lua
@@ -34,6 +34,20 @@ vim.opt.conceallevel = 1
 
 vim.opt.cursorline = true
 
+-- Neovide shizz
+-- Put anything you want to happen only in Neovide here
+-- Helper function for transparency formatting
+local alpha = function()
+	return string.format("%x", math.floor(255 * (vim.g.transparency or 0.9)))
+end
+-- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+vim.g.neovide_transparency = 0.9
+vim.g.transparency = 0
+vim.g.neovide_background_color = "#0f1117" .. alpha()
+vim.g.neovide_floating_blur_amount_x = 2.0
+vim.g.neovide_floating_blur_amount_y = 2.0
+vim.g.neovide_fullscreen = true
+
 -- Extra transparency~
 vim.g.transparent_groups = vim.list_extend(vim.g.transparent_groups or {}, {
 	"FloatBorder",
