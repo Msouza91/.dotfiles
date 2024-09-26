7cd2009b243685e8118d350ac415481bc690d06c
Marcos Souza <me@msouza.tech>
2024-09-26 13:00:14 -0300

    fix: commented out color scheme since it wasn't needed

--- .config/nvim/lua/marcos/plugins/barbecue.lua
+++ .config/nvim/lua/marcos/plugins/barbecue.lua
@@ -12,42 +12,42 @@ return {
 		require("barbecue").setup({
 			attach_navic = false, -- prevent barbecue from automatically attaching nvim-navic
 			theme = {
-				normal = { fg = "#faf4f4" }, -- Text
+				--normal = { fg = "#faf4f4" }, -- Text
 
-				ellipsis = { fg = "#7874b2" }, -- Muted
-				separator = { fg = "#7874b2" }, -- Muted
-				modified = { fg = "#7874b2" }, -- Muted
+				--ellipsis = { fg = "#7874b2" }, -- Muted
+				--separator = { fg = "#7874b2" }, -- Muted
+				--modified = { fg = "#7874b2" }, -- Muted
 
-				dirname = { fg = "#d6c1f3" }, -- Subtle
-				basename = { bold = true },
+				--dirname = { fg = "#d6c1f3" }, -- Subtle
+				--basename = { bold = true },
 				context = {},
 
-				context_file = { fg = "#9ccfd8" }, -- Foam
-				context_module = { fg = "#9ccfd8" }, -- Foam
-				context_namespace = { fg = "#9ccfd8" }, -- Foam
-				context_package = { fg = "#9ccfd8" }, -- Foam
-				context_class = { fg = "#9ccfd8" }, -- Foam
-				context_method = { fg = "#9ccfd8" }, -- Foam
-				context_property = { fg = "#9ccfd8" }, -- Foam
-				context_field = { fg = "#9ccfd8" }, -- Foam
-				context_constructor = { fg = "#9ccfd8" }, -- Foam
-				context_enum = { fg = "#9ccfd8" }, -- Foam
-				context_interface = { fg = "#9ccfd8" }, -- Foam
-				context_function = { fg = "#9ccfd8" }, -- Foam
-				context_variable = { fg = "#9ccfd8" }, -- Foam
-				context_constant = { fg = "#9ccfd8" }, -- Foam
-				context_string = { fg = "#9ccfd8" }, -- Foam
-				context_number = { fg = "#9ccfd8" }, -- Foam
-				context_boolean = { fg = "#9ccfd8" }, -- Foam
-				context_array = { fg = "#9ccfd8" }, -- Foam
-				context_object = { fg = "#9ccfd8" }, -- Foam
-				context_key = { fg = "#9ccfd8" }, -- Foam
-				context_null = { fg = "#9ccfd8" }, -- Foam
-				context_enum_member = { fg = "#9ccfd8" }, -- Foam
-				context_struct = { fg = "#9ccfd8" }, -- Foam
-				context_event = { fg = "#9ccfd8" }, -- Foam
-				context_operator = { fg = "#9ccfd8" }, -- Foam
-				context_type_parameter = { fg = "#9ccfd8" }, -- Foam
+				--context_file = { fg = "#9ccfd8" }, -- Foam
+				--context_module = { fg = "#9ccfd8" }, -- Foam
+				--context_namespace = { fg = "#9ccfd8" }, -- Foam
+				--context_package = { fg = "#9ccfd8" }, -- Foam
+				--context_class = { fg = "#9ccfd8" }, -- Foam
+				--context_method = { fg = "#9ccfd8" }, -- Foam
+				--context_property = { fg = "#9ccfd8" }, -- Foam
+				--context_field = { fg = "#9ccfd8" }, -- Foam
+				--context_constructor = { fg = "#9ccfd8" }, -- Foam
+				--context_enum = { fg = "#9ccfd8" }, -- Foam
+				--context_interface = { fg = "#9ccfd8" }, -- Foam
+				--context_function = { fg = "#9ccfd8" }, -- Foam
+				--context_variable = { fg = "#9ccfd8" }, -- Foam
+				--context_constant = { fg = "#9ccfd8" }, -- Foam
+				--context_string = { fg = "#9ccfd8" }, -- Foam
+				--context_number = { fg = "#9ccfd8" }, -- Foam
+				--context_boolean = { fg = "#9ccfd8" }, -- Foam
+				--context_array = { fg = "#9ccfd8" }, -- Foam
+				--context_object = { fg = "#9ccfd8" }, -- Foam
+				--context_key = { fg = "#9ccfd8" }, -- Foam
+				--context_null = { fg = "#9ccfd8" }, -- Foam
+				--context_enum_member = { fg = "#9ccfd8" }, -- Foam
+				--context_struct = { fg = "#9ccfd8" }, -- Foam
+				--context_event = { fg = "#9ccfd8" }, -- Foam
+				--context_operator = { fg = "#9ccfd8" }, -- Foam
+				--context_type_parameter = { fg = "#9ccfd8" }, -- Foam
 			},
 			create_autocmd = false, -- prevent barbecue from updating itself automatically
 		})
