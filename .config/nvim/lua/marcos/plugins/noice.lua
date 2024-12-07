46b33984b213f32ad6c2e98afe24754e5fe9e22a
Marcos Souza <me@msouza.tech>
2024-12-07 13:56:56 -0300

    feat: add snacks replacing notify and dashboard

--- .config/nvim/lua/marcos/plugins/noice.lua
+++ .config/nvim/lua/marcos/plugins/noice.lua
@@ -4,10 +4,6 @@ return {
 	dependencies = {
 		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 		"MunifTanjim/nui.nvim",
-		-- OPTIONAL:
-		--   `nvim-notify` is only needed, if you want to use the notification view.
-		--   If not available, we use `mini` as the fallback
-		"rcarriga/nvim-notify",
 	},
 	config = function()
 		require("noice").setup({
@@ -43,9 +39,6 @@ return {
 					["{%S-}"] = "@parameter",
 				},
 			},
-		})
-		require("notify").setup({
-			background_colour = "#191724",
 		})
 	end,
 }
