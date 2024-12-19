deb8f711816a10e373a1ceba1e14394318268c1d
Marcos Souza <me@msouza.tech>
2024-12-19 16:26:29 -0300

    feat: cleaner remaps

--- .config/nvim/lua/marcos/core/remap.lua
+++ .config/nvim/lua/marcos/core/remap.lua
@@ -2,102 +2,77 @@
 
 local utils = require("marcos.utils")
 
+local keymap = vim.keymap.set
+
 -- Set leader
 
 vim.g.mapleader = " "
 
--- Terminal
-vim.keymap.set("n", "<leader>tt", "<CMD> belowright 15sp term://$SHELL<CR>i")
-
 -- Netrw
-vim.keymap.set("n", "<leader>pv", "<CMD>NvimTreeToggle<CR>")
+keymap("n", "<leader>pv", "<CMD>NvimTreeToggle<CR>")
 
 -- Chg to current file directory
-vim.keymap.set("n", "<leader>cd", "<CMD>lcd %:p:h<CR>")
+keymap("n", "<leader>cd", "<CMD>lcd %:p:h<CR>")
 
 -- Move selected line / block of text in visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+keymap("v", "J", ":m '>+1<CR>gv=gv")
+keymap("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Better navigation
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+keymap("n", "J", "mzJ`z")
+keymap("n", "<C-d>", "<C-d>zz")
+keymap("n", "<C-u>", "<C-u>zz")
+keymap("n", "n", "nzzzv")
+keymap("n", "N", "Nzzzv")
 
 -- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
+keymap("x", "<leader>p", [["_dP]])
 
 -- Terminal Escape
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Escape from term-mode" })
+local opts = {}
+keymap("t", "jk", [[<C-\><C-n>]], opts)
 
 -- run current file
-vim.keymap.set("n", "<leader>r", utils.save_and_exec, { desc = "Save and execute file" })
+keymap("n", "<leader>r", utils.save_and_exec, { desc = "Save and execute file" })
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+keymap({ "n", "v" }, "<leader>y", [["+y]])
+keymap("n", "<leader>Y", [["+Y]])
 
 -- delete to black hole register
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+keymap({ "n", "v" }, "<leader>d", [["_d]])
 
 -- replace word under cursor
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- grant execute permission
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+keymap("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/.config/nvim/lua/marcos/plugins<CR>")
+keymap("n", "<leader>vpp", "<cmd>e ~/.dotfiles/.config/nvim/lua/marcos/plugins<CR>")
 
 -- source file
-vim.keymap.set("n", "<leader><leader>", function()
+keymap("n", "<leader><leader>", function()
 	vim.cmd("so")
 end)
 
 -- tmux sessionizer
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+keymap("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 -- Lazy window
-vim.keymap.set("n", "<leader>l", function()
+keymap("n", "<leader>l", function()
 	vim.cmd("Lazy")
 end)
 
 -- Lsp Formatting
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+keymap("n", "<leader>f", vim.lsp.buf.format)
 
 -- Quickfix navigation
-vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz")
-
--- Terraform shortcuts
-vim.keymap.set("n", "<leader>ti", ":!terraform init<CR>")
-vim.keymap.set("n", "<leader>tv", ":!terraform validate<CR>")
-vim.keymap.set("n", "<leader>tp", ":!terraform plan<CR>")
-
--- Telescope ChatGPT
-vim.keymap.set("n", "<leader>c", "<cmd>Telescope gpt<CR>")
-
--- Obsidian
-vim.keymap.set("n", "<leader>o", function()
-	vim.cmd("ObsidianSearch")
-end)
-vim.keymap.set("n", "<leader>od", function()
-	vim.cmd("ObsidianToday")
-end)
-vim.keymap.set("n", "<leader>oy", function()
-	vim.cmd("ObsidianYesterday")
-end)
-vim.keymap.set("n", "<leader>ot", function()
-	vim.cmd("ObsidianTomorrow")
-end)
-vim.keymap.set("n", "<leader>op", function()
-	vim.cmd("ObsidianPasteImg")
-end)
+keymap("n", "<C-j>", "<cmd>cnext<CR>zz")
+keymap("n", "<C-k>", "<cmd>cprev<CR>zz")
+keymap("n", "<leader>j", "<cmd>lnext<CR>zz")
+keymap("n", "<leader>k", "<cmd>lprev<CR>zz")
 
 -- MD Preview
-vim.keymap.set("n", "<leader>md", function()
+keymap("n", "<leader>md", function()
 	vim.cmd("MarkdownPreviewToggle")
 end)
