3d3675dba83efaa8cb16ede9f306adfb53540077
Marcos Souza <me@msouza.tech>
2024-12-25 17:19:18 -0300

    feat: locals for less typing when setting options

--- .config/nvim/lua/marcos/core/set.lua
+++ .config/nvim/lua/marcos/core/set.lua
@@ -1,48 +1,52 @@
-vim.opt.guicursor = ""
-vim.opt.nu = true
-vim.opt.relativenumber = true
+local opt = vim.opt
+local api = vim.api
+local glb = vim.g
+
+opt.guicursor = ""
+opt.nu = true
+opt.relativenumber = true
 
 -- No need to see the mode since I'm using statusline
-vim.opt.showmode = false
+opt.showmode = false
 
 -- Set splits to my liking
-vim.opt.splitbelow = true
-vim.opt.splitright = true
+opt.splitbelow = true
+opt.splitright = true
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
 
 -- Cursive italic comments haha
-vim.api.nvim_set_hl(0, "Comment", { italic = true })
+api.nvim_set_hl(0, "Comment", { italic = true })
 
-vim.opt.smartindent = true
-vim.opt.indentexpr = "nvim_treesitter#indent()"
+opt.smartindent = true
+opt.indentexpr = "nvim_treesitter#indent()"
 
-vim.opt.wrap = false
+opt.wrap = false
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
 
-vim.opt.incsearch = true
+opt.incsearch = true
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+opt.scrolloff = 8
+opt.signcolumn = "yes"
+opt.isfname:append("@-@")
 
-vim.opt.updatetime = 50
+opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+opt.colorcolumn = "80"
 
-vim.opt.conceallevel = 2
+opt.conceallevel = 2
 
-vim.opt.cursorline = true
+opt.cursorline = true
 
 -- Extra transparency~
-vim.g.transparent_groups = vim.list_extend(vim.g.transparent_groups or {}, {
+glb.transparent_groups = vim.list_extend(glb.transparent_groups or {}, {
 	"FloatBorder",
 	"NormalFloat",
 })
