434164a8f47ddbbfd95da59f43bb923baf551b0d
Marcos Souza <me@msouza.tech>
2024-12-28 01:00:14 -0300

    feat: better scroll with more neat animations

--- .config/nvim/lua/marcos/plugins/neoscrool.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"karb94/neoscroll.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		require("neoscroll").setup({
-			mappings = { "<C-u>", "<C-d>", "<C-y>", "zt", "zz", "zb" },
-		})
-	end,
-}
