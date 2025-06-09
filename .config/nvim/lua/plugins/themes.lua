844d6f84356da6780d6748ca57b7df6b703e6e73
Marcos Souza <marcos.souza@dtidigital.com.br>
2025-06-09 01:00:14 -0300

    ayu and auto ld

--- .config/nvim/lua/plugins/themes.lua
+++ .config/nvim/lua/plugins/themes.lua
@@ -1,11 +1,30 @@
 return {
-	"neanias/everforest",
-	name = "everforest",
-	lazy = false,
-	config = function()
-		vim.cmd("colorscheme everforest")
-		--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		vim.g.everforest_background = "hard"
-	end,
+  "Shatur/neovim-ayu",
+  name = "ayu",
+  lazy = false,
+  config = function()
+    local opts = {
+      mirage = false,
+      terminal = true,
+      overrides = {},
+    }
+    require("ayu").setup(opts)
+    if vim.o.background == "dark" then
+      vim.cmd("colorscheme ayu-dark")
+    else
+      vim.cmd("colorscheme ayu-light")
+    end
+  end,
 }
+
+-- return {
+-- 	"neanias/everforest",
+-- 	name = "everforest",
+-- 	lazy = false,
+-- 	config = function()
+-- 		vim.cmd("colorscheme everforest")
+-- 		--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- 		--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- 		vim.g.everforest_background = "hard"
+-- 	end,
+-- }
