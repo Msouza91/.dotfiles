4e2299ef68ab1e7468e87844461a50efd0482a3d
Marcos Souza <me@msouza.tech>
2025-06-20 18:40:51 -0300

    ld wall

--- .config/aerospace/presentation.sh
+++ .config/aerospace/presentation.sh
@@ -12,12 +12,20 @@ if [ $1 == "on" ]; then
     sed -i '' "s/\(outer\.top = *\[.*\), $top_normal_pixel_gaps]/\1, $presentation_pixel_gaps]/" ~/.config/aerospace/aerospace.toml
     sed -i '' "s/\(outer\.bottom = *\[.*\), $bottom_normal_pixel_gaps]/\1, $presentation_pixel_gaps]/" ~/.config/aerospace/aerospace.toml
     sed -i '' "s/size.*/size=$terminal_presentation_font_size/" ~/.config/alacritty/alacritty.toml
-    osascript -e 'tell application "System Events" to set picture of every desktop to "/Users/stefan.pinter/Pictures/Wallpapers/Brp_wallpaper_blue.png"'
+    if [ "$(dark-notify -e)" = "dark" ]; then
+        osascript -e 'tell application "System Events" to set picture of every desktop to "/Users/marcos/Downloads/nafdnzphc38ee5yibrsx.png"'
+    else
+        osascript -e 'tell application "System Events" to set picture of every desktop to "/Users/marcos/Downloads/qtxk7cprgi4ck1p2wilj.png"'
+    fi
     aerospace reload-config
 elif [ $1 == "off" ]; then
     sed -i '' "s/\(outer\.top = *\[.*\), $presentation_pixel_gaps]/\1, $top_normal_pixel_gaps]/" ~/.config/aerospace/aerospace.toml
     sed -i '' "s/\(outer\.bottom = *\[.*\), $presentation_pixel_gaps]/\1, $bottom_normal_pixel_gaps]/" ~/.config/aerospace/aerospace.toml
     sed -i '' "s/size.*/size=$terminal_font_size/" ~/.config/alacritty/alacritty.toml
-    osascript -e 'tell application "System Events" to set picture of every desktop to "/Users/stefan.pinter/.config/bluering.png"'
+    if [ "$(dark-notify -e)" = "dark" ]; then
+        osascript -e 'tell application "System Events" to set picture of every desktop to "/Users/marcos/Downloads/Wall/IMG_6926.jpg"'
+    else
+        osascript -e 'tell application "System Events" to set picture of every desktop to "/Users/marcos/Downloads/Wall/IMG_6925.jpg"'
+    fi
     aerospace reload-config
 fi
